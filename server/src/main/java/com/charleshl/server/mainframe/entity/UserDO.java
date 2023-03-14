c8520f724572b4648aae72b45c934cad95d0d583
Charles HL <charles.hildle@gmail.com>
2023-03-14 17:23:40 +0100

    List of all todos a user

--- server/src/main/java/com/charleshl/server/mainframe/entity/UserDO.java
+++ server/src/main/java/com/charleshl/server/mainframe/entity/UserDO.java
@@ -1,10 +1,13 @@
 package com.charleshl.server.mainframe.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.Data;
 
 
 @Entity
 @Table(name = "users", schema = "myschema")
+@Data
 public class UserDO {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,36 +17,13 @@ public class UserDO {
     private String username;
 
     @Column(name = "password", columnDefinition = "VARCHAR(255)")
+    @JsonIgnore
     private String password;
 
     public UserDO() {}
 
     public UserDO(String username, String password) {
         this.username = username;
-        this.password = password;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
     }
 }
